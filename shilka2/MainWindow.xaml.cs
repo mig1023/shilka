6bd05883a5858f7ada4ba534fcc6ff3664f8a0a6
mig1024 <mig1024@yandex.ru>
2018-03-31 21:17:38 +0300

    primitive start menu

--- shilka2/MainWindow.xaml.cs
+++ shilka2/MainWindow.xaml.cs
@@ -22,20 +22,6 @@ namespace shilka2
         {
             InitializeComponent();
 
-            Game = new System.Timers.Timer(30);
-            Game.Enabled = true;
-            Game.Elapsed += new ElapsedEventHandler(Shell.ShellsFire);
-            Game.Elapsed += new ElapsedEventHandler(Shell.ShellsFly);
-            Game.Elapsed += new ElapsedEventHandler(Case.CasesFly);
-            Game.Elapsed += new ElapsedEventHandler(Aircraft.AircraftFly);
-            Game.Elapsed += new ElapsedEventHandler(Shilka.StatisticShow);
-            Game.Start();
-
-            Aircrafts = new System.Timers.Timer(2000);
-            Aircrafts.Enabled = true;
-            Aircrafts.Elapsed += new ElapsedEventHandler(Aircraft.AircraftStart);
-            Aircrafts.Start();
-
             this.WindowState = System.Windows.WindowState.Maximized;
             this.WindowStyle = System.Windows.WindowStyle.None;
 
@@ -51,7 +37,38 @@ namespace shilka2
             StatisticMenu.Width = System.Windows.SystemParameters.PrimaryScreenWidth;
             StatisticMenu.Margin = new Thickness(0, System.Windows.SystemParameters.PrimaryScreenHeight, 0, 0);
 
+            StartMenu.Height = StatisticMenu.Height;
+            StartMenu.Width = StatisticMenu.Width;
+            StartMenu.Margin = new Thickness(0, 0, 0, 0);
+
+            var converter = new BrushConverter();
+            StartMenu.Background = (Brush)converter.ConvertFrom("#FF03034B");
+
             ShilkaImg.Margin = new Thickness(0, heightForShilka, 0, 0);
+        }
+
+        public void StartGame()
+        {
+            double l = StartMenu.Margin.Left - StartMenu.ActualWidth;
+            double t = StartMenu.Margin.Top;
+            double r = StartMenu.Margin.Right;
+            double b = StartMenu.Margin.Bottom;
+
+            MoveCanvas(StartMenu, l, t, r, b, 1);
+
+            Game = new System.Timers.Timer(30);
+            Game.Enabled = true;
+            Game.Elapsed += new ElapsedEventHandler(Shell.ShellsFire);
+            Game.Elapsed += new ElapsedEventHandler(Shell.ShellsFly);
+            Game.Elapsed += new ElapsedEventHandler(Case.CasesFly);
+            Game.Elapsed += new ElapsedEventHandler(Aircraft.AircraftFly);
+            Game.Elapsed += new ElapsedEventHandler(Shilka.StatisticShow);
+            Game.Start();
+
+            Aircrafts = new System.Timers.Timer(2000);
+            Aircrafts.Enabled = true;
+            Aircrafts.Elapsed += new ElapsedEventHandler(Aircraft.AircraftStart);
+            Aircrafts.Start();
         }
 
         public void EndGame(string endText, string bgColor)
@@ -71,7 +88,7 @@ namespace shilka2
             var converter = new BrushConverter();
             EndMenu.Background = (Brush)converter.ConvertFrom(bgColor);
 
-            MoveCanvas(EndMenu, l, t, r, b);
+            MoveCanvas(EndMenu, l, t, r, b, 0.2);
         }
 
         public void GameStatisticShow()
@@ -92,7 +109,7 @@ namespace shilka2
             StatisticGrid.Height = StatisticMenu.Height - Shilka.statisticGridMargins;
             StatisticGrid.Width = StatisticMenu.Width - Shilka.statisticGridMargins;
 
-            MoveCanvas(StatisticMenu, l, t, r, b);
+            MoveCanvas(StatisticMenu, l, t, r, b, 0.2);
         }
 
         public void ReturnStatisticShow()
@@ -102,16 +119,16 @@ namespace shilka2
             double r = StatisticMenu.Margin.Right;
             double b = StatisticMenu.Margin.Bottom;
 
-            MoveCanvas(StatisticMenu, l, t, r, b);
+            MoveCanvas(StatisticMenu, l, t, r, b, 0.2);
 
             Game.Start();
             Aircrafts.Start();
         }
 
-        public void MoveCanvas(Canvas moveCanvas, double l, double t, double r, double b)
+        public void MoveCanvas(Canvas moveCanvas, double l, double t, double r, double b, double speed)
         {
             ThicknessAnimation move = new ThicknessAnimation();
-            move.Duration = TimeSpan.FromSeconds(0.2);
+            move.Duration = TimeSpan.FromSeconds(speed);
             move.From = moveCanvas.Margin;
             move.To = new Thickness(l, t, r, b);
             moveCanvas.BeginAnimation(Border.MarginProperty, move);
@@ -188,6 +205,11 @@ namespace shilka2
         private void returnButton_Click(object sender, RoutedEventArgs e)
         {
             ReturnStatisticShow();
+        }
+
+        private void startButton_Click(object sender, RoutedEventArgs e)
+        {
+            StartGame();
         }
     }
 }
