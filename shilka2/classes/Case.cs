f554a2de111be05763115f9b99702792f4157e65
mig1024 <mig1024@yandex.ru>
2018-05-26 21:12:48 +0300

    cases free fall

--- shilka2/classes/Case.cs
+++ shilka2/classes/Case.cs
@@ -11,6 +11,8 @@ namespace shilka2
 {
     class Case : FlyObject
     {
+        double fall = 0;
+
         const int CASE_LENGTH = 5;
         const double MIN_FRAGM_SIN = 0.2;
         const double MAX_FRAGM_SIN = 0.4;
@@ -18,7 +20,8 @@ namespace shilka2
         const double MAX_FRAGM_COS = 0.8;
         const int MIN_SPEED = 3;
         const int MAX_SPEED = 12;
-        const int EXTR_HEIGHT_CORRECTION = 18; 
+        const int EXTR_HEIGHT_CORRECTION = 18;
+        const double FREE_FALL_SPEED = 0.05;
 
         public int speed { get; set; }
 
@@ -82,8 +85,10 @@ namespace shilka2
 
                     shellTrace.Stroke = Brushes.Black;
 
+                    c.fall += FREE_FALL_SPEED;
+
                     c.x = (c.x - c.speed * c.cos);
-                    c.y = (c.y - c.speed * c.sin);
+                    c.y = (c.y - c.speed * c.sin) + c.fall;
 
                     if (c.x < 0)
                         c.fly = false;
