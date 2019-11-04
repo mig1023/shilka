64a70edaad07e3ac7c73bff9dd3c094d8575fd93
mig1023 <mig1023@yandex.ru>
2019-11-04 22:01:50 +0300

    dead code removed

--- shilka2/Constants.cs
+++ shilka2/Constants.cs
@@ -35,7 +35,6 @@ namespace shilka2
         public const int CLOUD_WIDTH_MAX = 501;
         public const int CLOUD_HEIGHT_MIN = 70;
         public const int CLOUD_HEIGHT_MAX = 171;
-        // size = new int[] { Aircraft.rand.Next(Constants.CLOUD_WIDTH_MIN, Constants.CLOUD_WIDTH_MAX), Aircraft.rand.Next(Constants.CLOUD_HEIGHT_MIN, Constants.CLOUD_HEIGHT_MAX) },
 
         // case
         public const int CASE_LENGTH = 2;
